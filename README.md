bf0e92d2fb366197271127b5daeb62ea2b45bee0
Homology1996 <karta2211845@gmail.com>
2024-03-24 21:36:18 +0800

    20240324: Deploy war package mechanism

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # MySideProject
+
+1: 如果啟動Server時出現ClassNotFoundException，此時可以對專案按右鍵 -> Maven -> Update Project
+2: 對專案按右鍵 -> Run As -> Maven install，此時會在target資料夾裡面產生war檔。
+若無法順利執行，可以先到Window -> Preference -> Maven -> Installation -> 加入新的Maven處理程序
--- src/main/java/com/example/demo/MySideProjectApplication.java
+++ src/main/java/com/example/demo/MySideProjectApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 // 加上Scan之後才會去讀取指定的package
@@ -11,7 +12,7 @@ import org.springframework.context.annotation.ComponentScan;
 @EntityScan({"com.example.controller", "com.example.dao", "com.example.dao.mapper", "com.example.dao.mapper",
 	"com.example.db", "com.example.demo", "com.example.model", "com.example.service"})
 @SpringBootApplication
-public class MySideProjectApplication {
+public class MySideProjectApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MySideProjectApplication.class, args);
