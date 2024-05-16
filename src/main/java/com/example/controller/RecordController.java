09aeacecd737f3a6e0650f3d1a92789a25853930
Homology1996 <karta2211845@gmail.com>
2024-05-16 20:04:27 +0800

    20240516: Remove unused import

--- src/main/java/com/example/controller/RecordController.java
+++ src/main/java/com/example/controller/RecordController.java
@@ -9,7 +9,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
-import org.json.JSONArray;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- src/main/java/com/example/demo/MySideProjectApplication.java
+++ src/main/java/com/example/demo/MySideProjectApplication.java
@@ -7,9 +7,9 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.annotation.ComponentScan;
 
 // 加上Scan之後才會去讀取指定的package
-@ComponentScan({"com.example.controller", "com.example.dao", "com.example.dao.mapper", "com.example.dao.mapper",
+@ComponentScan({"com.example.controller", "com.example.dao", "com.example.dao.mapper",
 	"com.example.db", "com.example.demo", "com.example.model", "com.example.service"})
-@EntityScan({"com.example.controller", "com.example.dao", "com.example.dao.mapper", "com.example.dao.mapper",
+@EntityScan({"com.example.controller", "com.example.dao", "com.example.dao.mapper",
 	"com.example.db", "com.example.demo", "com.example.model", "com.example.service"})
 @SpringBootApplication
 public class MySideProjectApplication extends SpringBootServletInitializer {
